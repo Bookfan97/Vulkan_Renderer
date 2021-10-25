8e7c0fe2237e5791b83a7d59cddbdae436353cc8
Noah Day <sheltielover432@gmail.com>
2021-10-25 00:27:22 -0400

    Fragment Interpolation - Vulkan Game Engine Tutorial 07
    CU-1q2hr9b

--- VulkanRenderer/lve_model.cpp
+++ VulkanRenderer/lve_model.cpp
@@ -13,15 +13,20 @@ namespace lve
 
 	std::vector<VkVertexInputAttributeDescription> LveModel::Vertex::getAttributeDescriptions()
 	{
-		std::vector<VkVertexInputAttributeDescription> attributeDescriptions(1);
+		std::vector<VkVertexInputAttributeDescription> attributeDescriptions(2);
 		attributeDescriptions[0].binding = 0;
 		attributeDescriptions[0].location = 0;
 		attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
-		attributeDescriptions[0].offset = 0;
+		attributeDescriptions[0].offset = offsetof(Vertex, position);
+
+		attributeDescriptions[1].binding = 0;
+		attributeDescriptions[1].location = 1;
+		attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+		attributeDescriptions[1].offset = offsetof(Vertex, color);
 		return attributeDescriptions;
 	}
 
-	LveModel::LveModel(LveDevice& device, const std::vector<Vertex>& vertices): lveDevice(device)
+	LveModel::LveModel(LveDevice& device, const std::vector<Vertex>& vertices) : lveDevice(device)
 	{
 		createVertexBuffers(vertices);
 	}
@@ -34,14 +39,14 @@ namespace lve
 
 	void LveModel::bind(VkCommandBuffer commandBuffer)
 	{
-		VkBuffer buffers[] = {vertexBuffer};
-		VkDeviceSize offsets[] = {0};
-		vkCmdBindVertexBuffers(commandBuffer, 0 ,1, buffers, offsets);
+		VkBuffer buffers[] = { vertexBuffer };
+		VkDeviceSize offsets[] = { 0 };
+		vkCmdBindVertexBuffers(commandBuffer, 0, 1, buffers, offsets);
 	}
 
 	void LveModel::draw(VkCommandBuffer commandBuffer)
 	{
-		vkCmdDraw(commandBuffer, vertexCount, 1, 0,0);
+		vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
 	}
 
 	void LveModel::createVertexBuffers(const std::vector<Vertex>& vertices)
@@ -50,16 +55,15 @@ namespace lve
 		assert(vertexCount >= 3 && "VERTEX COUNT MUST BE AT LEAST 3");
 		VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
 		lveDevice.createBuffer(
-			bufferSize, 
-			VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, 
+			bufferSize,
+			VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
 			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
 			vertexBuffer,
 			vertexBufferMemory
 		);
-		void *data;
+		void* data;
 		vkMapMemory(lveDevice.device(), vertexBufferMemory, 0, bufferSize, 0, &data);
 		memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
 		vkUnmapMemory(lveDevice.device(), vertexBufferMemory);
 	}
-	
 }
